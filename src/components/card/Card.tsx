e7407aaf7e883681536e60c7007b8f68a76706e5
hiroji <joegilbertdev@outlook.com>
2025-09-06 17:09:44 +0100

    Added cards

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,16 @@
+import styles from './Card.module.css';
+
+interface CardProps {
+    title: string;
+    textContent: string;
+}
+
+export const Card = ({title, textContent}: CardProps) => {
+    return (
+        <article className={styles.card}>
+            <h4 className={styles.title}>{title}</h4>
+            <p className={styles.textContent}>{textContent}</p>
+        </article>
+    )
+
+}
---
+++ src/main-view/competencies/Competencies.tsx
@@ -0,0 +1,11 @@
+import {Cards} from './Cards';
+import styles from './Competencies.module.css';
+
+export const Competencies = () => {
+    return (
+        <>
+            <h2 className={styles.h2}>Skills and competencies</h2>
+            <Cards/>
+        </>
+    )
+};
