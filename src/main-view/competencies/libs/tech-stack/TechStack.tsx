83b60ab3765da1047519995339ae55846b388d01
hiroji <joegilbertdev@outlook.com>
2025-09-07 12:36:32 +0100

    basic tech stack

--- src/main-view/competencies/libs/tech-stack/TechStack.tsx
+++ src/main-view/competencies/libs/tech-stack/TechStack.tsx
@@ -4,6 +4,7 @@ import styles from "./TechStack.module.css";
 import { Dropdown } from "@/components";
 import { Dispatch, SetStateAction, useState } from "react";
 import { TechnologyType } from "./technology-type";
+import {techItems} from './tech-items';
 
 export const TechStack = () => {
   const [filterBy, setFilterBy] = useState(TechnologyType.All);
@@ -15,14 +16,24 @@ export const TechStack = () => {
   return (
     <div className={styles.container}>
       <div className={styles.horizontalFlexBox}>
-        <p className={styles.text}>I have skills in the following: </p>
+        <p className={styles.text}>Filter my technology skills by: </p>
         <Dropdown
           name="select to filter technologies by"
           defaultValue={filterBy}
           setValue={setFilterBy as Dispatch<SetStateAction<string>>}
           options={Object.values(TechnologyType)}
         />
+
+
       </div>
+
+        <div className={styles.gridContainer}>
+            {
+                techItems
+                    .filter(({type}) => type === filterBy || filterBy === TechnologyType.All)
+                    .map(({name}, index) => <article className={styles.gridItem} key={index}>{name}</article>)
+            }
+        </div>
     </div>
   );
 };
---
+++ src/main-view/competencies/libs/tech-stack/tech-items.ts
@@ -0,0 +1,74 @@
+export const techItems = [
+    {
+        type: "Languages",
+        name: "JavaScript"
+    },
+    {
+        type: "Languages",
+        name: "TypeScript"
+    },
+    {
+        type: "Languages",
+        name: "CSS"
+    },
+    {
+        type: "Languages",
+        name: "HTML5"
+    },
+    {
+        type: "Languages",
+        name: "Python"
+    },
+    {
+        type: "Languages",
+        name: "C#"
+    },
+    {
+        type: "Back end",
+        name: "Azure Databricks"
+    },
+    {
+        type: "Back end",
+        name: "dotNet"
+    },
+    {
+        type: "Back end",
+        name: "MSSQL"
+    },
+    {
+        type: "GenAI",
+        name: "Ollama"
+    },
+    {
+        type: "GenAI",
+        name: "Microsoft Copilot"
+    },
+    {
+        type: "GenAI",
+        name: "LangChain"
+    },
+    {
+        type: "GenAI",
+        name: "LangSmith"
+    },
+    {
+        type: "Front end",
+        name: "NextJs"
+    },
+    {
+        type: "Front end",
+        name: "React"
+    },
+    {
+        type: "Front end",
+        name: "Jest"
+    },
+    {
+        type: "Front end",
+        name: "Playwright"
+    },
+    {
+        type: "Front end",
+        name: "Cypress"
+    }
+]
