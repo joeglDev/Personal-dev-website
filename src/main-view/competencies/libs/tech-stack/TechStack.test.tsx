365d8aa8860a4fa8a49f3312363defce0bdbba71
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2025-09-08 19:08:36 +0100

    Added a react component test for <TechStack/> (#3)

---
+++ src/main-view/competencies/libs/tech-stack/TechStack.test.tsx
@@ -0,0 +1,126 @@
+import "@testing-library/jest-dom";
+import { TechStack } from "./TechStack";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("<TechStack/>", () => {
+  test("displays default all state", () => {
+    const { getByRole, getByText } = render(<TechStack />);
+
+    const dropdown = getByRole("combobox");
+    expect(dropdown).toBeInTheDocument();
+    expect(dropdown).toHaveTextContent("All");
+
+    // genAI
+    expect(getByText("Ollama")).toBeInTheDocument();
+
+    // Front end
+    expect(getByText("React")).toBeInTheDocument();
+
+    // backend
+    expect(getByText("dotNet")).toBeInTheDocument();
+
+    // languages
+    expect(getByText("Python")).toBeInTheDocument();
+  });
+
+  test("can filter by 'GenAI'", async () => {
+    const user = userEvent.setup();
+    const { getByRole, getByText, queryByText } = render(<TechStack />);
+
+    const dropdown = getByRole("combobox");
+    expect(dropdown).toBeInTheDocument();
+    expect(dropdown).toHaveTextContent("All");
+
+    // click new option
+    await user.click(dropdown);
+    await user.selectOptions(getByRole("combobox"), "GenAI");
+
+    // genAI
+    expect(getByText("Ollama")).toBeInTheDocument();
+
+    // Front end
+    expect(queryByText("React")).not.toBeInTheDocument();
+
+    // backend
+    expect(queryByText("dotNet")).not.toBeInTheDocument();
+
+    // languages
+    expect(queryByText("Python")).not.toBeInTheDocument();
+  });
+
+  test("can filter by 'Front end'", async () => {
+    const user = userEvent.setup();
+    const { getByRole, getByText, queryByText } = render(<TechStack />);
+
+    const dropdown = getByRole("combobox");
+    expect(dropdown).toBeInTheDocument();
+    expect(dropdown).toHaveTextContent("All");
+
+    // click new option
+    await user.click(dropdown);
+    await user.selectOptions(getByRole("combobox"), "Front end");
+
+    // genAI
+    expect(queryByText("Ollama")).not.toBeInTheDocument();
+
+    // Front end
+    expect(getByText("React")).toBeInTheDocument();
+
+    // backend
+    expect(queryByText("dotNet")).not.toBeInTheDocument();
+
+    // languages
+    expect(queryByText("Python")).not.toBeInTheDocument();
+  });
+
+  test("can filter by 'Back end'", async () => {
+    const user = userEvent.setup();
+    const { getByRole, getByText, queryByText } = render(<TechStack />);
+
+    const dropdown = getByRole("combobox");
+    expect(dropdown).toBeInTheDocument();
+    expect(dropdown).toHaveTextContent("All");
+
+    // click new option
+    await user.click(dropdown);
+    await user.selectOptions(getByRole("combobox"), "Back end");
+
+    // genAI
+    expect(queryByText("Ollama")).not.toBeInTheDocument();
+
+    // Front end
+    expect(queryByText("React")).not.toBeInTheDocument();
+
+    // backend
+    expect(getByText("dotNet")).toBeInTheDocument();
+
+    // languages
+    expect(queryByText("Python")).not.toBeInTheDocument();
+  });
+
+  test("can filter by 'Languages'", async () => {
+    const user = userEvent.setup();
+    const { getByRole, getByText, queryByText } = render(<TechStack />);
+
+    const dropdown = getByRole("combobox");
+    expect(dropdown).toBeInTheDocument();
+    expect(dropdown).toHaveTextContent("All");
+
+    // click new option
+    await user.click(dropdown);
+    await user.selectOptions(getByRole("combobox"), "Languages");
+
+    // genAI
+    expect(queryByText("Ollama")).not.toBeInTheDocument();
+
+    // Front end
+    expect(queryByText("React")).not.toBeInTheDocument();
+
+    // backend
+    expect(queryByText("dotNet")).not.toBeInTheDocument();
+
+    // languages
+    expect(getByText("Python")).toBeInTheDocument();
+  });
+});
