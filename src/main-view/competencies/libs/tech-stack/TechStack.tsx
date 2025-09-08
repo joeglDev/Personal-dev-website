fb32d250d5eb14dccb3e96be79b98439218750c1
hiroji <joegilbertdev@outlook.com>
2025-09-08 19:07:15 +0100

    Added a react component test for <TechStack/>

--- src/main-view/competencies/libs/tech-stack/TechStack.tsx
+++ src/main-view/competencies/libs/tech-stack/TechStack.tsx
@@ -1,23 +1,29 @@
 "use client";
 
 import styles from "./TechStack.module.css";
-import { Dropdown } from "@/components";
+import { Dropdown } from "../../../../components/dropdown";
 import { Dispatch, SetStateAction, useState } from "react";
 import { TechnologyType } from "./technology-type";
-import {techItems} from './tech-items';
+import { techItems } from "./tech-items";
 import Image from "next/image";
 
 interface TechItemProps {
-    name: string;
-    path: string;
+  name: string;
+  path: string;
 }
 
-const TechItem = ({ name, path}: TechItemProps) => (
-    <li  className={styles.gridItem}>
-        <span>{name}</span>
-        <Image className={styles.gridImage} src={`/tech-stack-svgs/${path}`} height={64} width={64} alt={`${name} logo`}/>
-    </li>
-)
+const TechItem = ({ name, path }: TechItemProps) => (
+  <li className={styles.gridItem}>
+    <span>{name}</span>
+    <Image
+      className={styles.gridImage}
+      src={`/tech-stack-svgs/${path}`}
+      height={64}
+      width={64}
+      alt={`${name} logo`}
+    />
+  </li>
+);
 
 export const TechStack = () => {
   const [filterBy, setFilterBy] = useState(TechnologyType.All);
@@ -36,17 +42,17 @@ export const TechStack = () => {
           setValue={setFilterBy as Dispatch<SetStateAction<string>>}
           options={Object.values(TechnologyType)}
         />
-
-
       </div>
 
-        <ul className={styles.gridContainer}>
-            {
-                techItems
-                    .filter(({type}) => type === filterBy || filterBy === TechnologyType.All)
-                    .map(({name, path}, index) => <TechItem key={index} name={name} path={path ?? ''} />)
-            }
-        </ul>
+      <ul className={styles.gridContainer}>
+        {techItems
+          .filter(
+            ({ type }) => type === filterBy || filterBy === TechnologyType.All,
+          )
+          .map(({ name, path }, index) => (
+            <TechItem key={index} name={name} path={path ?? ""} />
+          ))}
+      </ul>
     </div>
   );
 };
--- src/main-view/competencies/libs/tech-stack/tech-items.ts
+++ src/main-view/competencies/libs/tech-stack/tech-items.ts
@@ -1,92 +1,92 @@
 export const techItems = [
-    {
-        type: "Languages",
-        name: "JavaScript",
-        path: 'js.svg'
-    },
-    {
-        type: "Languages",
-        name: "TypeScript",
-        path: 'ts.svg'
-    },
-    {
-        type: "Languages",
-        name: "CSS",
-        path: 'css.svg'
-    },
-    {
-        type: "Languages",
-        name: "HTML5",
-        path: 'html.svg'
-    },
-    {
-        type: "Languages",
-        name: "Python",
-        path: 'python.svg'
-    },
-    {
-        type: "Languages",
-        name: "C#",
-        path: 'csharp.svg'
-    },
-    {
-        type: "Back end",
-        name: "Azure Databricks",
-        path: 'databricks.svg'
-    },
-    {
-        type: "Back end",
-        name: "dotNet",
-        path: 'dotnet.svg'
-    },
-    {
-        type: "Back end",
-        name: "MSSQL",
-        path: 'sql.svg'
-    },
-    {
-        type: "GenAI",
-        name: "Ollama",
-        path: 'llama.svg'
-    },
-    {
-        type: "GenAI",
-        name: "Microsoft Copilot",
-        path: 'ai.svg'
-    },
-    {
-        type: "GenAI",
-        name: "LangChain",
-        path: 'parrot.svg'
-    },
-    {
-        type: "GenAI",
-        name: "LangSmith",
-        path: 'parrot.svg'
-    },
-    {
-        type: "Front end",
-        name: "NextJs",
-        path: 'nextjs.svg'
-    },
-    {
-        type: "Front end",
-        name: "React",
-        path: 'react.svg'
-    },
-    {
-        type: "Front end",
-        name: "Jest",
-        path: 'jest.svg'
-    },
-    {
-        type: "Front end",
-        name: "Playwright",
-        path: 'playwright.svg'
-    },
-    {
-        type: "Front end",
-        name: "Cypress",
-        path: 'cypress.svg'
-    }
-]
+  {
+    type: "Languages",
+    name: "JavaScript",
+    path: "js.svg",
+  },
+  {
+    type: "Languages",
+    name: "TypeScript",
+    path: "ts.svg",
+  },
+  {
+    type: "Languages",
+    name: "CSS",
+    path: "css.svg",
+  },
+  {
+    type: "Languages",
+    name: "HTML5",
+    path: "html.svg",
+  },
+  {
+    type: "Languages",
+    name: "Python",
+    path: "python.svg",
+  },
+  {
+    type: "Languages",
+    name: "C#",
+    path: "csharp.svg",
+  },
+  {
+    type: "Back end",
+    name: "Azure Databricks",
+    path: "databricks.svg",
+  },
+  {
+    type: "Back end",
+    name: "dotNet",
+    path: "dotnet.svg",
+  },
+  {
+    type: "Back end",
+    name: "MSSQL",
+    path: "sql.svg",
+  },
+  {
+    type: "GenAI",
+    name: "Ollama",
+    path: "llama.svg",
+  },
+  {
+    type: "GenAI",
+    name: "Microsoft Copilot",
+    path: "ai.svg",
+  },
+  {
+    type: "GenAI",
+    name: "LangChain",
+    path: "parrot.svg",
+  },
+  {
+    type: "GenAI",
+    name: "LangSmith",
+    path: "parrot.svg",
+  },
+  {
+    type: "Front end",
+    name: "NextJs",
+    path: "nextjs.svg",
+  },
+  {
+    type: "Front end",
+    name: "React",
+    path: "react.svg",
+  },
+  {
+    type: "Front end",
+    name: "Jest",
+    path: "jest.svg",
+  },
+  {
+    type: "Front end",
+    name: "Playwright",
+    path: "playwright.svg",
+  },
+  {
+    type: "Front end",
+    name: "Cypress",
+    path: "cypress.svg",
+  },
+];
