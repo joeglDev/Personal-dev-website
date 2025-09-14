36aa70c23d77965a89b01af77ca2502c30b7ef84
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2025-09-14 22:57:31 +0100

    Feat: Add intro (#7)
        Adds an introduction container and profile Image to the main page
        Clears up todo
        Changed use of colours on main page for alternating colours

---
+++ src/main-view/introduction/Introduction.tsx
@@ -0,0 +1,28 @@
+import styles from "./Introduction.module.css";
+import Image from "next/image";
+
+export const Introduction = () => {
+  const altText =
+    "A photo of a man. He is taking a selfie of himself and looking at the camera. He has pink hair and is wearing a grey jumper with silver necklace. The background is the cityscape of London looking towards the Shard. Tags: man";
+  return (
+    <section className={styles.introductionContainer}>
+      <div className={styles.verticalFlexBox}>
+        <h1 className={styles.introductionTitle}>Hi, I&apos;m Joe Gilbert</h1>
+        <p className={styles.introductionText}>
+          A full-stack developer based in North-West England.
+        </p>
+        <p className={styles.introductionText}>Busy with 🪴 | 🐈‍⬛ | 💻</p>
+      </div>
+      <Image
+        src="/images/me.jpg"
+        alt={altText}
+        quality={100}
+        width={800}
+        height={600}
+        style={{ margin: "auto", borderRadius: "10%", maxHeight: "600px", maxWidth: "800px" }}
+        layout="responsive"
+        sizes="(max-width: 768px) 100vw, (max-width: 1080px) 100vw,  20vw"
+      />
+    </section>
+  );
+};
