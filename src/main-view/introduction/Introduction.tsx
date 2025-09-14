5ce509dd8ebaed016c88ca1a2e8a4ba840431d87
hiroji <joegilbertdev@outlook.com>
2025-09-14 22:20:35 +0100

    Added alt test to image

--- src/main-view/introduction/Introduction.tsx
+++ src/main-view/introduction/Introduction.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 // add image
 export const Introduction = () => {
-    const altText = "";
+    const altText = "A photo of a man. He is taking a selfie of himself and looking at the camera. He has pink hair and is wearing a grey jumper with silver necklace. The background is the cityscape of London looking towards the Shard. Tags: man";
     return (
         <section className={styles.introductionContainer}>
             <div className={styles.verticalFlexBox}>
