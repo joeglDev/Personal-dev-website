1cd948378b0ae040d93bc6e1e1c1e1e2cce5ce86
Joe Gilbert <joegilbertdev@outlook.com>
2025-09-14 14:00:24 +0100

    Updated metadata for pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 import { Competencies } from "@/main-view/competencies";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Joe Gilbert Full-stack Developer",
+  description:
+    "A portfolio and living CV for Joe Gilbert Full-stack software developer. Contains a list of competencies in TypeScript, React, NextJs, Python and C# dotNet.",
+};
 
 export default function Home() {
   return (
--- src/app/perlin-noise/page.tsx
+++ src/app/perlin-noise/page.tsx
@@ -1,4 +1,11 @@
 import { PerlinNoise } from "@/perlin-noise";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Perlin noise demo",
+  description:
+    "A 2d Perlin Noise generator which uses NextJs client rendering to create pixelated maps in React.",
+};
 
 export default function Page() {
   return <PerlinNoise />;
