79e40814e04fea57ebea53a3b466451680bbd705
Joe Gilbert <joegilbertdev@outlook.com>
2025-09-14 14:15:59 +0100

    added basic navigation to header

--- src/share/header/Header.tsx
+++ src/share/header/Header.tsx
@@ -1,9 +1,11 @@
 import { LightDarkModeToggle } from "@/share/header/LightDarkModeToggle";
 import styles from "./Header.module.css";
+import { Navigation } from "./Navigation";
 
 export const Header = () => {
   return (
     <header className={styles.horizontalFlexBox}>
+      <Navigation/>
       <LightDarkModeToggle />
     </header>
   );
