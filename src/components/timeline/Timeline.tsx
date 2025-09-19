9ed33204afa1084df544b959f4b8d7d01057285d
hiroji <joegilbertdev@outlook.com>
2025-09-19 15:11:18 +0100

    Moved TimelineItem content to new react component

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,23 +1,26 @@
-import { TimelineItem } from "./timeline.types";
+import { TimelineItemProps, TimelineProps} from "./timeline.types";
 import styles from "./Timeline.module.css";
 
-interface TimelineProps {
-  items: TimelineItem[];
+const TimelineItem = (props: TimelineItemProps) => {
+    const {index, title, content} = props;
+    return (
+        <article
+            key={index}
+            className={index % 2 ? styles.containerLeft : styles.containerRight}
+        >
+            <div className={styles.content}>
+                <h3>{title}</h3>
+                <p>{content}</p>
+            </div>
+        </article>
+    )
 }
 
 export const Timeline = ({ items }: TimelineProps) => {
   return (
     <div className={styles.timeline}>
       {items.map(({ title, content }, index) => (
-        <article
-          key={index}
-          className={index % 2 ? styles.containerLeft : styles.containerRight}
-        >
-          <div className={styles.content}>
-            <h3>{title}</h3>
-            <p>{content}</p>
-          </div>
-        </article>
+        <TimelineItem key={index} index={index} title={title} content={content}/>
       ))}
     </div>
   );
--- src/components/timeline/timeline.types.ts
+++ src/components/timeline/timeline.types.ts
@@ -1,4 +1,12 @@
-export type TimelineItem = {
+type TimelineItem = {
   title: string;
   content: string; // this could be made ReactNode to pass children
 };
+
+export interface TimelineItemProps extends TimelineItem {
+    index: number;
+}
+
+export interface TimelineProps {
+    items: TimelineItem[];
+}
