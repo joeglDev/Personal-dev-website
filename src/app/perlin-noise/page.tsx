e87071e205b5a190e8e3614ab1a7006235fcd696
hiroji <joegilbertdev@outlook.com>
2025-09-27 19:22:27 +0100

    Updated metadata of pages

--- src/app/perlin-noise/page.tsx
+++ src/app/perlin-noise/page.tsx
@@ -4,7 +4,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Perlin noise demo",
   description:
-    "A 2d Perlin Noise generator which uses NextJs client rendering to create pixelated maps in React.",
+    "A 2d Perlin Noise generator demo which uses NextJs client rendering to create pixelated maps in React.",
+  keywords: ["perlin noise", "demo", "2d perlin noise", "react", "nextjs"],
+  authors: [
+    { name: "Joe Gilbert", url: "https://www.linkedin.com/in/joegilbertdev/" },
+  ],
 };
 
 export default function Page() {
