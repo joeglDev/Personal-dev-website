f6634e2c738d47e61d0d859d3fa2b1a27439be11
hiroji <joegilbertdev@outlook.com>
2025-09-27 19:04:44 +0100

    Set loading of introduction image to priority

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Joe Gilbert Full-stack Developer",
   description:
-    "A portfolio and living CV for Joe Gilbert Full-stack software developer. Contains a list of competencies in TypeScript, React, NextJs, Python and C# dotNet.",
+    "A portfolio and living CV for Joe Gilbert Full-stack software developer. Contains an introduction, a list of technical competencies, career history and contact information.",
 };
 
 export default function Home() {
